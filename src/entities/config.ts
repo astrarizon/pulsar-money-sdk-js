09da9924ed26f9014d423281c97429781d85593f
Adrian Larionescu <astrarizon@gmail.com>
2023-09-06 01:08:48 +0300

    started extracting core methods

--- src/entities/config.ts
+++ src/entities/config.ts
@@ -1,44 +1,51 @@
 interface IBASE_URLS {
-	mainnet: string;
-	devnet: string;
-	testnet: string;
+  mainnet: string;
+  devnet: string;
+  testnet: string;
 }
 
 export const BASE_URLS: IBASE_URLS = {
-	mainnet: 'https://pulsar-money-prod.herokuapp.com',
-	devnet: 'https://pulsar-money-devnet.herokuapp.com',
-	testnet: 'https://pulsar-money-testnet.herokuapp.com',
-};
-export type FEATURE_TYPE = 'create_payment' | 'claim_payment' | 'cancel_payment' | 'delegate';
-
-export const getContractAddress = (chain: 'testnet' | 'devnet' | 'mainnet') => {
-	switch (chain) {
-		case 'testnet':
-			throw new Error('Not implemented');
-		case 'devnet':
-			return 'erd1qqqqqqqqqqqqqpgqsanann348xhns6qx94rgcq8davw005vnlzhsezyt7t';
-		case 'mainnet':
-			return 'erd1qqqqqqqqqqqqqpgqd6l8ayd0zxfekl53geyxgjzjxu3ceyca60wsje6asx';
-		default:
-			throw new Error(`Invalid chain. ${chain}`);
-	}
+  mainnet: "https://pulsar-money-prod.herokuapp.com",
+  devnet: "https://pulsar-money-devnet.herokuapp.com",
+  testnet: "https://pulsar-money-testnet.herokuapp.com",
 };
 
-export const getFeatureUrl = (chain: 'testnet' | 'devnet' | 'mainnet', feature: FEATURE_TYPE) => {
-	const url = BASE_URLS[chain];
+export const MultiversxApiUrls = {
+  mainnet: "https://api.multiversx.com",
+  devnet: "https://devnet-api.multiversx.com",
+  testnet: "https://testnet-api.elrond.com",
+};
 
-	switch (feature) {
-		case 'create_payment':
-			return `${url}${CREATE_PULSAR_PAYMENT_ENDPOINT}`;
-		case 'claim_payment':
-			return `${url}${CLAIM_PULSAR_PAYMENT_ENDPOINT}`;
-		case 'cancel_payment':
-			return `${url}${CANCEL_PULSAR_PAYMENT_ENDPOINT}`;
-		case 'delegate':
-			return `${url}${DELEGATE_ENDPOINT}`;
-		default:
-			throw new Error(`Invalid feature type. ${feature}`);
-	}
+export type FEATURE_TYPE = "create_payment" | "claim_payment" | "cancel_payment" | "delegate";
+
+export const getContractAddress = (chain: "testnet" | "devnet" | "mainnet") => {
+  switch (chain) {
+    case "testnet":
+      throw new Error("Not implemented");
+    case "devnet":
+      return "erd1qqqqqqqqqqqqqpgqsanann348xhns6qx94rgcq8davw005vnlzhsezyt7t";
+    case "mainnet":
+      return "erd1qqqqqqqqqqqqqpgqd6l8ayd0zxfekl53geyxgjzjxu3ceyca60wsje6asx";
+    default:
+      throw new Error(`Invalid chain. ${chain}`);
+  }
+};
+
+export const getFeatureUrl = (chain: "testnet" | "devnet" | "mainnet", feature: FEATURE_TYPE) => {
+  const url = BASE_URLS[chain];
+
+  switch (feature) {
+    case "create_payment":
+      return `${url}${CREATE_PULSAR_PAYMENT_ENDPOINT}`;
+    case "claim_payment":
+      return `${url}${CLAIM_PULSAR_PAYMENT_ENDPOINT}`;
+    case "cancel_payment":
+      return `${url}${CANCEL_PULSAR_PAYMENT_ENDPOINT}`;
+    case "delegate":
+      return `${url}${DELEGATE_ENDPOINT}`;
+    default:
+      throw new Error(`Invalid feature type. ${feature}`);
+  }
 };
 
 export const CREATE_PULSAR_PAYMENT_ENDPOINT = `/transaction/payment/create`;
@@ -48,3 +55,6 @@ export const GET_FEE_ENDPOINT = `/transaction/payment/get-fee`;
 export const DELEGATE_ENDPOINT = `/transaction/stake`;
 
 export const DEFAULT_FREQUENCY_SECONDS = 1;
+
+export const ONE_SECOND_IN_MILISECONDS = 1000;
+export const GAS_LIMIT = 12000000;
---
+++ src/entities/transactions-utils/transaction-utils.ts
@@ -0,0 +1,92 @@
+import {
+  Account,
+  GasEstimator,
+  IAddress,
+  INonce,
+  IPlainTransactionObject,
+  ITransactionValue,
+  TokenTransfer,
+  Transaction,
+  TransactionPayload,
+  TransferTransactionsFactory,
+} from "@multiversx/sdk-core/out";
+import { GAS_LIMIT } from "../config";
+import { TransactionFactory, getAccount, getNetworkProvider } from "../utils";
+
+export class TransactionsUtils {
+  static async createSendEgld(
+    amount: number,
+    senderAddress: IAddress,
+    receiverAddress: IAddress,
+    currentChain: "testnet" | "devnet" | "mainnet",
+    message = ""
+  ) {
+    const extraGasIfNeeded = message ? message.length * 1500 : 0;
+
+    const egldAmount = TokenTransfer.egldFromAmount(amount);
+    const networkProvider = getNetworkProvider(currentChain);
+
+    const senderAccount = await getAccount(senderAddress.bech32(), networkProvider);
+
+    return this.createFinalTransaction(
+      senderAccount.nonce,
+      egldAmount,
+      receiverAddress,
+      senderAccount.address,
+      GAS_LIMIT + extraGasIfNeeded,
+      new TransactionPayload(message),
+      currentChain
+    );
+  }
+
+  static async createSendEsdtTx(
+    tokenId: string,
+    amount: number,
+    decimals: number,
+    senderAccount: Account,
+    receiverAddress: IAddress,
+    extraPayload = "",
+    currentChain: "testnet" | "devnet" | "mainnet"
+  ) {
+    const payment = TokenTransfer.fungibleFromAmount(tokenId, amount, decimals);
+    const payload = TransactionFactory.createESDTTransfer({
+      tokenTransfer: payment,
+      nonce: senderAccount.nonce,
+      receiver: receiverAddress,
+      sender: senderAccount.address,
+      chainID: currentChain,
+    });
+
+    const finalPayload = extraPayload ? `${payload.getData()}@${extraPayload}` : `${payload.getData()}`;
+
+    return this.createFinalTransaction(
+      senderAccount.nonce,
+      undefined,
+      receiverAddress,
+      senderAccount.address,
+      GAS_LIMIT,
+      new TransactionPayload(finalPayload),
+      currentChain
+    );
+  }
+
+  private static createFinalTransaction(
+    nonce: INonce,
+    value: ITransactionValue | undefined,
+    receiver: IAddress,
+    sender: IAddress,
+    gasLimit: number,
+    data: TransactionPayload,
+    chainID: string
+  ): IPlainTransactionObject {
+    return new Transaction({
+      nonce,
+      value,
+      receiver,
+      sender,
+      gasLimit,
+      data,
+      chainID,
+    }).toPlainObject();
+  }
+}
